67a8038013cb6f6fe73baf0fe24e0bf951b54569
霖弟弟 <agilebeanisartist@gmail.com>
2021-01-12 10:57:25 +0800

    Create Consumer.java

---
+++ multithread/producerandconsumerproblem/Consumer.java
@@ -0,0 +1,39 @@
+package com.concurrency.producerandconsumerproblem;
+
+import java.util.List;
+
+public class Consumer implements Runnable {
+
+    private int number;
+
+    private List<String> breads;
+
+    public Consumer(int number, List<String> breads) {
+        this.number = number;
+        this.breads = breads;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (breads) {
+                if (breads.size() == 0) {
+                    try {
+                        breads.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    System.out.println("consumer" + number + " consume the number " + (breads.size() - 1) + " bread");
+                    breads.remove(breads.size() - 1);
+                }
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                breads.notifyAll();
+            }
+        }
+    }
+}
