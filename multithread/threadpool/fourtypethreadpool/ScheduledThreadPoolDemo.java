3f6467c043b88af256c0e5f75acdb856c29b863b
霖弟弟 <agilebeanisartist@gmail.com>
2021-01-14 10:52:14 +0800

    Create ScheduledThreadPoolDemo.java

---
+++ multithread/threadpool/fourtypethreadpool/ScheduledThreadPoolDemo.java
@@ -0,0 +1,30 @@
+package com.concurrency.threadpool.fourtypethreadpool;
+
+import java.util.concurrent.*;
+
+/**
+ * 相当于带定时功能的FixedThreadPool
+ */
+public class ScheduledThreadPoolDemo {
+
+    public static void main(String[] args) {
+        ScheduledThreadPoolExecutor executor = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(3);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+
+                }
+            }
+        };
+        for (int i = 0; i < 10; i++) {
+            executor.schedule(runnable, 1, TimeUnit.SECONDS);
+        }
+
+        while (true) {
+            System.out.print("活跃线程数:" + (executor).getActiveCount());
+            System.out.println(" 队列任务数:" + (executor).getQueue().size());
+        }
+
+    }
+}
