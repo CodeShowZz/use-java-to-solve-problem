fda373e9822ca9a1258cf2b38784697ef9ff436d
huangjunlin <573930212@qq.com>
2021-05-06 08:00:50 +0800

    commit

--- src/main/java/algorithm/sort/HeapSort.java
+++ src/main/java/algorithm/sort/HeapSort.java
@@ -1,11 +1,9 @@
 package algorithm.sort;
 
-import edu.princeton.cs.algs4.Heap;
-
 import java.util.Arrays;
 
 /**
- * 目前记录:7分35秒
+ *  堆排序
  *
  * @author junlin_huang
  * @create 2021-03-24 下午7:55
@@ -14,36 +12,43 @@ import java.util.Arrays;
 public class HeapSort {
 
     public static void main(String[] args) {
-        int[] arr = {5, 8, 4, 2, 6};
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] nums = {1, 6, 4, 3, 7};
+        new HeapSort().heapSort(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
-    public static void sort(int[] arr) {
-        int length = arr.length;
+    public void heapSort(int[] nums) {
+        int length = nums.length;
         for (int i = length / 2 - 1; i >= 0; i--) {
-            heapify(arr, i, length);
+            treeify(nums, i, length);
         }
-        for (int i = length - 1; i >= 0; i--) {
-            CommonUtil.swap(arr,0, i);
-            heapify(arr,0,i);
+        for (int i = length - 1; i > 0; i--) {
+            CommonUtil.swap(nums, 0, i);
+            treeify(nums, 0, i);
         }
+    }
+
+    public void buildHeap() {
 
     }
 
-    public static void heapify(int[] arr, int i, int n) {
+
+    public void treeify(int[] nums, int i, int length) {
+        int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
-        int largest = i;
-        if (left < n && arr[left] > arr[largest]) {
+        if (left > length - 1 || right > length - 1) {
+            return;
+        }
+        if (nums[left] > nums[largest]) {
             largest = left;
         }
-        if (right < n && arr[right] > arr[largest]) {
+        if (nums[right] > nums[largest]) {
             largest = right;
         }
         if (i != largest) {
-            CommonUtil.swap(arr, i, largest);
-            heapify(arr, largest, n);
+            CommonUtil.swap(nums, i, largest);
+            treeify(nums, largest, length);
         }
     }
 }
